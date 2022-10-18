ded1580b6ea862218f4583e7084581aaa6cd5244
Henry Gilbert <77060624+henrygilbert22@users.noreply.github.com>
2022-10-18 17:11:34 -0500

    added data and model trainer

---
+++ final_project/model_trainer.py
@@ -0,0 +1,126 @@
+from matplotlib import pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox, TextArea
+import matplotlib
+import numpy as np
+import pandas as pd
+import torch
+import torchvision
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils import data
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm, tqdm_notebook
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from typing import Tuple
+from model import CVAE
+
+SEED = 42
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+def load_mnist_data(batch_size: int, transform: transforms.Compose) -> Tuple[data.DataLoader, data.DataLoader]:
+    
+    dataset = MNIST('./data', transform=transform, download=True)
+    train_data, test_data = data.random_split(dataset, (50000,10000))
+
+    train_dataset = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_dataset = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    return train_dataset, val_dataset
+    
+def plot_loss(history):
+    loss, val_loss = zip(*history)
+    plt.figure(figsize=(15, 9))
+    plt.plot(loss, label="train_loss")
+    plt.plot(val_loss, label="val_loss")
+    plt.legend(loc='best')
+    plt.xlabel("epochs")
+    plt.ylabel("loss")
+    plt.show()
+
+def one_hot(x, max_x):
+    return torch.eye(max_x + 1)[x]
+
+def vae_loss_fn(x, recon_x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
+
+def evaluate(losses, autoencoder, dataloader, device: torch.device, flatten=True):
+    
+    model = lambda x, y: autoencoder(x, y)[0]    
+    loss_sum = []
+    loss_fn = nn.MSELoss()
+    
+    for inputs, labels in dataloader:
+        inputs = inputs.to(device)
+        labels = one_hot(labels,9).to(device)
+
+        if flatten:
+            inputs = inputs.view(inputs.size(0), 28*28)
+
+        outputs = model(inputs, labels)
+        loss = loss_fn(inputs, outputs)            
+        loss_sum.append(loss)
+
+    losses.append((sum(loss_sum)/len(loss_sum)).item())
+    
+def train_model(net, dataloader, test_dataloader, flatten=True, epochs=20):
+    validation_losses = []
+    optim = torch.optim.Adam(net.parameters())
+
+    log_template = "\nEpoch {ep:03d} val_loss {v_loss:0.4f}"
+    with tqdm(desc="epoch", total=epochs) as pbar_outer:  
+        for i in range(epochs):
+            for batch, labels in dataloader:
+                batch = batch.to(DEVICE)
+                labels = one_hot(labels,9).to(DEVICE)
+
+                if flatten:
+                    batch = batch.view(batch.size(0), 28*28)
+
+                optim.zero_grad()
+                x,mu,logvar = net(batch, labels)
+                loss = vae_loss_fn(batch, x[:, :784], mu, logvar)
+                loss.backward()
+                optim.step()
+            evaluate(validation_losses, net, test_dataloader, DEVICE, flatten=True)
+            pbar_outer.update(1)
+            tqdm.write(log_template.format(ep=i+1, v_loss=validation_losses[i]))
+    plt.show()
+    return validation_losses
+
+def main():
+    
+    cvae = CVAE(28*28, 20, 10).to(DEVICE)
+    train_dataset, val_dataset = load_mnist_data(128, transform)
+    history = train_model(cvae, train_dataset, val_dataset)
+
+    val_loss = history
+    plt.figure(figsize=(15, 9))
+    plt.plot(val_loss, label="val_loss")
+    plt.legend(loc='best')
+    plt.xlabel("epochs")
+    plt.ylabel("loss")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+    
+    
